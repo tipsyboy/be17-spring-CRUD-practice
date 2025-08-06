918ab0c961cbfaaef02471ab687031fd6cf9c3af
kbw <kbwook96@naver.com>
2025-08-06 15:59:57 +0900

    hello

--- crud/src/main/java/study/crud/lecture/model/LectureEntity.java
+++ crud/src/main/java/study/crud/lecture/model/LectureEntity.java
@@ -1,9 +1,11 @@
 package study.crud.lecture.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class LectureEntity {
 
     @Id
