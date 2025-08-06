99146ac658d765a2eeec8a4e8aefd5780cb39902
seoyoen22 <hsy2709@gmail.com>
2025-08-06 11:38:01 +0900

    movie service

---
+++ crud/src/main/java/study/crud/movie/service/MovieService.java
@@ -0,0 +1,38 @@
+package study.crud.movie.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+    private final MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository){
+        this.movieRepository = movieRepository;
+    }
+
+    public void register(MovieDto.Register dto){
+        movieRepository.save(dto.toEntity());
+    }
+
+    public List<MovieDto.Movie> list() {
+        List<MovieEntity> result = movieRepository.findAll();
+        return result.stream().map(MovieDto.Movie::from).toList();
+    }
+
+    public MovieDto.Movie read(Integer idx){
+        Optional<MovieEntity> result = movieRepository.findById(idx);
+
+        if(result.isPresent()){
+            MovieEntity entity = result.get();
+            return MovieDto.Movie.from(entity);
+        }
+        return null;
+    }
+
+    public List<MovieDto.Movie> search(String name) {
+        List<MovieEntity> result = movieRepository.findByName(name);
+        return result.stream().map(MovieDto.Movie::from).toList();
+    }
+}
