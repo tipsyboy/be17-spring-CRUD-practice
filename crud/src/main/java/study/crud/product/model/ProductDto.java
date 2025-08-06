bd7cfb8a1db15d6b1df29c704510269d17b9c3a8
seoyoen22 <hsy2709@gmail.com>
2025-08-06 11:04:58 +0900

    product dto

---
+++ crud/src/main/java/study/crud/product/model/ProductDto.java
@@ -0,0 +1,92 @@
+package study.crud.product.model;
+
+public class ProductDto {
+    public static class Product {
+        private Integer idx;
+        private String name;
+        private String description;
+        private Integer price;
+
+        public static ProductDto.Product from(ProductEntity entity){
+            ProductDto.Product dto = new ProductDto.Product();
+            dto.setIdx(entity.getIdx());
+            dto.setName(entity.getName());
+            dto.setDescription(entity.getDescription);
+            dto.setPrice(entity.getPrice);
+
+            return dto;
+
+        }
+
+        public Integer getIdx() {
+            return idx;
+        }
+
+        public void setIdx(Integer idx) {
+            this.idx = idx;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public Integer getPrice() {
+            return price;
+        }
+
+        public void setPrice(Integer price) {
+            this.price = price;
+        }
+    }
+
+    public static class Register {
+        private String name;
+        private String description;
+        private Integer price;
+
+        public ProductEntity toEntity() {
+            ProductEntity entity = new ProductEntity();
+            entity.setName(name);
+            entity.setDescription(description);
+            entity.setPrice(price);
+
+            return entity;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public Integer getPrice() {
+            return price;
+        }
+
+        public void setPrice(Integer price) {
+            this.price = price;
+        }
+    }
+}
